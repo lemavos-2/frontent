a3258560530e63931ab1f8a103450e2d5c38ee46
lemavos-2 <h7s.rick@gmail.com>
2026-03-07 15:48:20 +0000

    frontend v2.4

--- src/main/java/tech/lemnova/continuum/domain/email/PasswordResetTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package tech.lemnova.continuum.domain.email;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface PasswordResetTokenRepository extends MongoRepository<PasswordResetToken, String> {
-    Optional<PasswordResetToken> findByToken(String token);
-}
